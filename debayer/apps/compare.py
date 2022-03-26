4fce19a9376cbc6a170c27903b98fd2af2e76f75
Christoph Heindl <christoph.heindl@gmail.com>
2022-03-26 12:37:21 +0100

    updated readme and results

--- debayer/apps/compare.py
+++ debayer/apps/compare.py
@@ -39,23 +39,72 @@ def main():
         k: deb(t).squeeze().permute(1, 2, 0).cpu().numpy() for k, deb in methods.items()
     }
 
-    fig = plt.figure(constrained_layout=True)
+    fig = plt.figure(constrained_layout=True, figsize=(12, 8))
     spec = gridspec.GridSpec(ncols=3, nrows=2, figure=fig)
 
     def show_image(fig, spec, row, col, img, title, share_ax=None):
         ax = fig.add_subplot(spec[row, col], sharey=share_ax, sharex=share_ax)
         ax.imshow(img, interpolation="nearest")
-        ax.set_title(title)
-        ax.tick_params(bottom=False, labelbottom=False, left=False, labelleft=False)
+        ax.set_title(title, size=8)
+        ax.tick_params(
+            bottom=False,
+            top=False,
+            left=False,
+            right=False,
+            labelleft=False,
+            labelbottom=False,
+        )
         return ax
 
-    ax0 = show_image(fig, spec, 0, 0, rgb_opencv, "OpenCV")
-    show_image(fig, spec, 0, 1, res["Debayer2x2"], "Debayer2x2", ax0)
-    show_image(fig, spec, 0, 2, res["DebayerSplit"], "DebayerSplit", ax0)
-    show_image(fig, spec, 1, 0, res["Debayer3x3"], "Debayer3x3", ax0)
-    show_image(fig, spec, 1, 1, res["Debayer5x5"], "Debayer5x5", ax0)
-    fig.suptitle("Comparison of Demosaicing Methods", size=20)
+    #
+    axcv = show_image(fig, spec, 0, 1, rgb_opencv, "OpenCV")
+    show_image(fig, spec, 0, 2, res["Debayer2x2"], "Debayer2x2", axcv)
+    show_image(fig, spec, 1, 0, res["DebayerSplit"], "DebayerSplit", axcv)
+    show_image(fig, spec, 1, 1, res["Debayer3x3"], "Debayer3x3", axcv)
+    show_image(fig, spec, 1, 2, res["Debayer5x5"], "Debayer5x5", axcv)
+
+    fig.suptitle("Comparison of Demosaicing Methods", size=10)
     spec.tight_layout(fig, rect=[0, 0, 1, 0.97])
+
+    # axzoom = show_image(fig, spec, 0, 0, res["Debayer5x5"], "Zoom")
+    # def update_zoom(event_ax):
+    #     axzoom.clear()
+    #     axzoom.imshow(
+    #         rgb_opencv,
+    #         extent=(0, rgb_opencv.shape[1], rgb_opencv.shape[0], 0),
+    #         interpolation="nearest",
+    #         origin="upper",
+    #     )
+
+    #     left, right = event_ax.get_xlim()
+    #     bottom, top = event_ax.get_ylim()
+    #     axins = axzoom.inset_axes([0.5, 0.5, 0.47, 0.47])
+    #     axins.imshow(
+    #         rgb_opencv,
+    #         extent=(0, rgb_opencv.shape[1], rgb_opencv.shape[0], 0),
+    #         origin="upper",
+    #     )
+    #     axins.tick_params(
+    #         bottom=False,
+    #         labelbottom=False,
+    #         left=False,
+    #         labelleft=False,
+    #     )
+    #     axins.set_xlim(left, right)
+    #     axins.set_ylim(bottom, top)
+    #     axzoom.indicate_inset_zoom(axins, edgecolor="red")
+    #     axzoom.tick_params(
+    #         bottom=False,
+    #         top=False,
+    #         left=False,
+    #         right=False,
+    #         labelleft=False,
+    #         labelbottom=False,
+    #     )
+    # def on_ylims_change(event_ax):
+    #     update_zoom(event_ax)
+    # axcv.callbacks.connect("ylim_changed", on_ylims_change)
+
     plt.show()
 
 
