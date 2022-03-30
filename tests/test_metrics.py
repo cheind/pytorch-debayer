869c9874e46f1dd17c35824b4c754232c2a15dda
Christoph.Heindl <cheind@profactor.at>
2022-03-30 13:41:55 +0200

    added test to compare to opencv

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,5 +1,7 @@
 import torch
 import math
+import cv2
+
 from debayer.metrics import peak_signal_noise_ratio
 
 
@@ -35,3 +37,11 @@ def test_psnr():
         torch.tensor([1.0]).view(1, 1, 1), torch.tensor([8.0 / 10.0]).view(1, 1, 1), 1.0
     )
     assert torch.isclose(psnr, torch.tensor(20 - 10 * math.log10(4)))
+
+    # Compare to OpenCV
+    o = torch.randint(0, 255, size=(3, 200, 300), dtype=torch.uint8)
+    r = torch.randint(0, 255, size=(3, 200, 300), dtype=torch.uint8)
+
+    psnr, _ = peak_signal_noise_ratio(o.unsqueeze(0), r.unsqueeze(0), 255.0)
+    psnr_cv = cv2.PSNR(o.permute(1, 2, 0).numpy(), r.permute(1, 2, 0).numpy())
+    torch.isclose(psnr, torch.as_tensor(psnr_cv), atol=1e-3)
